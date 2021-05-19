c2ad146a1e78e8b5ffc7801fa2aebc409a58d797
cemarv25 <cesar.martinez0010@outlook.com>
2021-05-19 00:02:49 -0600

    AM-27 Implemented screen navigator

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
+import App from './App';
 import { AppRegistry } from 'react-native';
-import App from './App.js';
-import Signup from './js/screens/signup';
 
-AppRegistry.registerComponent('APP_NAME_HERE', () => Signup);
-
-// The below line is necessary for use with the TestBed App
-AppRegistry.registerComponent('ViroSample', () => Signup);
+AppRegistry.registerComponent('ViroSample', () => App);
